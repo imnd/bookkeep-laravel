01bf6d5b5e3d8fe962dd3e0509580733d0f54d18
Andrew Serdjuk <imndsu@gmail.com>
2022-12-01 16:01:54 +0530

    fix errors

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,8 @@ Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 Route::group([
     'middleware' => 'auth'
 ], function() {
-    Route::view('/', 'pages.index', ['page' => '']);
+    Route::view('/', 'pages.index');
+    Route::view('/home', 'pages.index');
     Route::view('settings', 'settings', ['page' => 'settings']);
     foreach (['articles', 'bills', 'clients', 'contracts', 'invoices', 'purchases'] as $entity) {
         Route::get("$entity/{any?}", function() use ($entity) {
--- tests/Feature/ArticleSubCatsTest.php
+++ tests/Feature/ArticleSubCatsTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 
-class ArticleSubcatsTest extends TestCase
+class ArticleSubCatsTest extends TestCase
 {
     /**
      * @inheritdoc
