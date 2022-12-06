8270fccef5e9e20043aafac886bd3fc08eb91628
Andrew Serdjuk <imndsu@gmail.com>
2022-12-06 18:47:07 +0530

    remove unused css code

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,8 +3,5 @@ const mix = require('laravel-mix');
 mix
     .js('resources/assets/js/app.js', 'public/js')
     .vue()
-    .sass('resources/assets/sass/app.scss', 'public/css')
-    // .postCss('resources/assets/sass/app.css', 'public/css', [
-    //     //
-    // ])
+    .postCss('resources/assets/sass/app.css', 'public/css')
 ;
